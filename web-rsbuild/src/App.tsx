e363509e2d250c9f788fded825c121df3d447e87
justajwolf <changbaihenx@163.com>
2024-09-27 16:14:10 +0800

    feat(): add antd

--- web-rsbuild/src/App.tsx
+++ web-rsbuild/src/App.tsx
@@ -1,12 +1,18 @@
+import React from "react";
+import { ConfigProvider, Button } from "antd";
+
 import "./App.css";
 
-const App = () => {
-  return (
+const App: React.FC = () => (
+  <ConfigProvider theme={{ token: { colorPrimary: "#00b96b" } }}>
     <div className="content">
       <h1>Rsbuild with React</h1>
       <p>Start building amazing things with Rsbuild.</p>
+      <Button id="btn" type="primary">
+        Button
+      </Button>
     </div>
-  );
-};
+  </ConfigProvider>
+);
 
 export default App;
